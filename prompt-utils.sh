0a2f1cf05c91abec3aa01725cb86f602d565092b
Joel Chen <jchip@users.noreply.github.com>
2016-09-17 19:50:36 -0700

    Update prompt-utils.sh

--- prompt-utils.sh
+++ prompt-utils.sh
@@ -3,5 +3,8 @@
 #
 
 function uh() {
-  history | sort -b -r -u | sort --key=1.8b -u -b | sort -u -b -n
+  history | sed -e 's/  *$//' | sort -b -r -u | sort --key=1.8b -u -b | sort -u -b -n | cut -b8- > /tmp/history_uh.$$
+  history -c -r /tmp/history_uh.$$
+  rm -f /tmp/history_uh.$$
+  history
 }
